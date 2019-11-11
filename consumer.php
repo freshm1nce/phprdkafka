d0c8e3827190553bb5df809a76074e6918a4f896
root <root@localhost.localdomain>
2019-11-11 15:20:38 +0300

    new working version

--- consumer.php
+++ consumer.php
@@ -1,27 +1,50 @@
-<?php
-$conf = new Conf();
-$conf->set('log_level', LOG_LEVEL);
-$conf->set('debug', 'all');
-$rk = new RdKafka\Consumer($conf);
-$rk->addBrokers("10.170.171.21:9093");
-$topic = $rk->newTopic("misp_event");
 
-// The first argument is the partition to consume from.
-// The second argument is the offset at which to start consumption. Valid values
-// are: RD_KAFKA_OFFSET_BEGINNING, RD_KAFKA_OFFSET_END, RD_KAFKA_OFFSET_STORED.
-$topic->consumeStart(0, RD_KAFKA_OFFSET_BEGINNING);
+<?php
+
+$conf = new RdKafka\Conf();
+
+// Set the group id. This is required when storing offsets on the broker
+$conf->set('group.id', 'myConsumerGroup');
+
+$rk = new RdKafka\Consumer($conf);
+$rk->addBrokers("172.16.41.1:9092");
+
+$topicConf = new RdKafka\TopicConf();
+$topicConf->set('auto.commit.interval.ms', 100);
+
+// Set the offset store method to 'file'
+$topicConf->set('offset.store.method', 'broker');
+
+// Alternatively, set the offset store method to 'none'
+// $topicConf->set('offset.store.method', 'none');
+
+// Set where to start consuming messages when there is no initial offset in
+// offset store or the desired offset is out of range.
+// 'smallest': start from the beginning
+$topicConf->set('auto.offset.reset', 'smallest');
+
+$topic = $rk->newTopic("test5", $topicConf);
+
+// Start consuming partition 0
+$topic->consumeStart(0, RD_KAFKA_OFFSET_STORED);
 
 while (true) {
-    // The first argument is the partition (again).
-    // The second argument is the timeout.
-    $msg = $topic->consume(0, 1000);
-    if (null === $msg) {
-        continue;
-    } elseif ($msg->err) {
-        echo $msg->errstr(), "\n";
-        break;
-    } else {
-        echo $msg->payload, "\n";
+    $message = $topic->consume(0, 120*10000);
+    switch ($message->err) {
+        case RD_KAFKA_RESP_ERR_NO_ERROR:
+            var_dump($message);
+            break;
+        case RD_KAFKA_RESP_ERR__PARTITION_EOF:
+            echo "No more messages; will wait for more\n";
+            break;
+        case RD_KAFKA_RESP_ERR__TIMED_OUT:
+            echo "Timed out\n";
+            break;
+        default:
+            throw new \Exception($message->errstr(), $message->err);
+            break;
     }
 }
+
 ?>
+
--- producer.php
+++ producer.php
@@ -1,11 +1,31 @@
+
 <?php
 
-$rk = new RdKafka\Producer();
-$rk->setLogLevel(LOG_DEBUG);
-$rk->addBrokers("10.170.171.21:9093");
+$conf = new RdKafka\Conf();
+$conf->set('metadata.broker.list', 'localhost:9092');
 
-$topic = $rk->newTopic("test");
+//If you need to produce exactly once and want to keep the original produce order, uncomment the line below
+//$conf->set('enable.idempotence', 'true');
+
+$producer = new RdKafka\Producer($conf);
+
+$topic = $producer->newTopic("test5");
 
 for ($i = 0; $i < 10; $i++) {
     $topic->produce(RD_KAFKA_PARTITION_UA, 0, "Message $i");
+    $producer->poll(0);
 }
+
+for ($flushRetries = 0; $flushRetries < 10; $flushRetries++) {
+    $result = $producer->flush(10000);
+    if (RD_KAFKA_RESP_ERR_NO_ERROR === $result) {
+        break;
+    }
+}
+
+if (RD_KAFKA_RESP_ERR_NO_ERROR !== $result) {
+    throw new \RuntimeException('Was unable to flush, messages might be lost!');
+}
+
+?>
+
